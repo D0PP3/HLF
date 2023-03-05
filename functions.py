cfc75fbf0d1d415a1693e85f251c233386c2f950
iker <irazabaliker4@gmail.com>
2023-03-05 23:54:02 +0100

    hlffff

--- functions.py
+++ functions.py
@@ -1,50 +1,74 @@
 import numpy as np
-from variables import tablero
+import random
 
+tablero = np.full((10, 10), " ")
 
-
-def colocar_barco(eslora):
-    np.random.seed(45)
-    coord_x_random = np.random.randint(10)
-    coord_y_random = np.random.randint(10)
-    print(coord_x_random)
-    print(coord_y_random)
-    orientacion = "O"
-    print(orientacion)
-
-    if orientacion == "E":
-
-        tablero[coord_x_random, coord_y_random : coord_y_random + eslora] = "O"
-        return tablero
-    if orientacion == "O":
-        tablero[coord_x_random, coord_y_random : coord_y_random - eslora] = "O"
-
-    if orientacion == "N":
-
-        tablero[coord_x_random, coord_y_random : coord_y_random + eslora] = "O"
-        
-    if orientacion == "S":
-        tablero[coord_x_random, coord_y_random : coord_y_random - eslora] = "O"
-
-
-def crear_tablero(size):
-    tablero = np.full((size,size), " ")
+def colocar_barcos(tablero, lista_barcos):
+    for barco in lista_barcos:
+        for pos in barco:
+            x, y = pos
+            tablero[x, y] = 'O'
     return tablero
 
-def disparar():
-    coor_jugador_x = int(input("Coordenada de disparo X: "))
-    coor_jugador_y = int(input("Coordenada de disparo Y: "))
+barco_1 = [(0,1)]
+barco_1_2 = [(0,3)]
+barco_1_3 = [(0,5)]
+barco_1_4 = [(0,7)]
+barco_2 = [(3,1),(3,2)]
+barco_2_2 = [(3,5),(3,6)]
+barco_2_3 = [(3,8), (3,9)]
+barco_3 = [(5,1),(5,2),(5,3)]
+barco_3_2 = [(5,7), (5,8), (5,9)]
+barco_4 = [(9,1), (9,2), (9,3), (9,4)]
 
-    if tablero[coor_jugador_x, coor_jugador_y] == "O":
-        print("Has acertado! Dispara otra vez.")
-        tablero[coor_jugador_x, coor_jugador_y] = "X"
-        print(tablero)
-    elif tablero[coor_jugador_x, coor_jugador_y] == " ":
-        print("Has fallado! Turno de la máquina.")
-        print(tablero)
-        tablero[coor_jugador_x, coor_jugador_y] = "-"
-    elif tablero[coor_jugador_x, coor_jugador_y] == "X" or tablero[coor_jugador_x, coor_jugador_y] == "-":
-        print("Ya has usado esta coordenada previamente, intenta otra vez.")
-        print(tablero)
+lista_barcos = [barco_1, barco_1_2, barco_1_3,
+                barco_1_4, barco_2, barco_2_2, 
+                barco_2_3, barco_3, barco_3_2, 
+                barco_4]
 
-tablero_jugador = crear_tablero(10)
+tablero_con_barcos = colocar_barcos(tablero, lista_barcos)
+
+# función para disparar al tablero
+def disparar(tablero, x, y):
+    if tablero[x, y] == 'O':
+        tablero[x, y] = 'X'
+        print("¡Le has dado a un barco!")
+        print(tablero) 
+        
+return True
+    elif tablero[x, y] == 'X':
+        print("Ya habías disparado ahí antes.")
+        print(tablero) 
+        return False
+    else:
+        tablero[x, y] = '-'
+        print("Agua.")
+        print(tablero) 
+        return False
+    
+# función para que la máquina dispare al tablero
+def disparar_maquina(tablero):
+    while True:
+        x = random.randint(0, 9)
+        y = random.randint(0, 9)
+        if tablero[x, y] == 'O':
+            tablero[x, y] = 'X'
+            print("La máquina te ha dado.")
+            print(tablero)
+            return True
+        elif tablero[x, y] == '-':
+            print("La máquina ha disparado en una posición que ya había disparado antes.")
+            print(tablero) 
+        else:
+            tablero[x, y] = '-'
+            print("La máquina ha disparado en agua.")
+            print(tablero) 
+            return False
+        
+# función para verificar si se han hundido todos los barcos
+def todos_barcos_hundidos(tablero):
+    for i in range(10):
+        for j in range(10):
+            if tablero[i][j] == 'O':
+                return False
+    return True
